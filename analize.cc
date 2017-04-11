ef6205de35cb69015972a972a62288616dd547d1
Luke <L.Trombach@massey.ac.nz>
2017-04-11 14:30:45 +1200

    removed test functions from analize

--- analize.cc
+++ analize.cc
@@ -137,16 +137,6 @@ int main (int argc, char *argv[]) {
 
 
 
-		////inter-particle distance
-		//vector<double> interPartDist;
-		//vector<coord3d> currentCoord=optKS[i].getCoordinates();
-		//for (vector<coord3d>::size_type j=0; j<currentCoord.size(); j++) {
-		//	for (vector<coord3d>::size_type k=j+1; k<currentCoord.size(); k++) {
-		//		interPartDist.push_back(coord3d::dist(currentCoord[j], currentCoord[k]));	
-		//	}
-		//}
-		//sort(interPartDist.begin(), interPartDist.end());
-		//optKS[i].setInterPartDist(interPartDist);
 		optKS[i].propertyInterPartDist();
 
 
@@ -182,129 +172,13 @@ int main (int argc, char *argv[]) {
 	cout << "\t\tTiming: " << T.timing() << " s" << endl << endl;
 
 
-
-	//SORT BY direct structure comparison
-	vector< vector<structure> > eqClasses_coord;
 	vector<structure> optKS0;
 	optKS0.push_back(optKS[0]);
-	eqClasses_coord.push_back(optKS0);
-	for (vector<structure>::size_type i = 1; i < optKS.size(); i++) {
-		bool matched(0);
-		for (vector< vector<structure> >::size_type j = 0; j < eqClasses_coord.size(); j++) {
-			if (optKS[i].compareCoordinates(eqClasses_coord[j][0])) {
-				eqClasses_coord[j].push_back(optKS[i]);
-				matched = 1;
-				break;
-			}
-			structure sig0;
-			vector<coord3d> coord_sig0 = optKS[i].sig(0);
-			sig0.setCoordinates(coord_sig0);
-			if (sig0.compareCoordinates(eqClasses_coord[j][0])) {
-				eqClasses_coord[j].push_back(optKS[i]);
-				matched = 1;
-				break;
-			}
-			structure sig1;
-			vector<coord3d> coord_sig1 = optKS[i].sig(1);
-			sig1.setCoordinates(coord_sig1);
-			if (sig1.compareCoordinates(eqClasses_coord[j][0])) {
-				eqClasses_coord[j].push_back(optKS[i]);
-				matched = 1;
-				break;
-			}
-			structure sig2;
-			vector<coord3d> coord_sig2 = optKS[i].sig(2);
-			sig2.setCoordinates(coord_sig2);
-			if (sig2.compareCoordinates(eqClasses_coord[j][0])) {
-				eqClasses_coord[j].push_back(optKS[i]);
-				matched = 1;
-				break;
-			}
-			structure c201;
-			vector<coord3d> coord_c201 = optKS[i].c2(0,1);
-			c201.setCoordinates(coord_c201);
-			if (c201.compareCoordinates(eqClasses_coord[j][0])) {
-				eqClasses_coord[j].push_back(optKS[i]);
-				matched = 1;
-				break;
-			}
-			structure c202;
-			vector<coord3d> coord_c202 = optKS[i].c2(0,2);
-			c202.setCoordinates(coord_c202);
-			if (c202.compareCoordinates(eqClasses_coord[j][0])) {
-				eqClasses_coord[j].push_back(optKS[i]);
-				matched = 1;
-				break;
-			}
-			structure c212;
-			vector<coord3d> coord_c212 = optKS[i].c2(1,2);
-			c212.setCoordinates(coord_c212);
-			if (c212.compareCoordinates(eqClasses_coord[j][0])) {
-				eqClasses_coord[j].push_back(optKS[i]);
-				matched = 1;
-				break;
-			}
-			structure inverse;
-			vector<coord3d> coord_inv = optKS[i].inv();
-			inverse.setCoordinates(coord_inv);
-			if (inverse.compareCoordinates(eqClasses_coord[j][0])) {
-				eqClasses_coord[j].push_back(optKS[i]);
-				matched = 1;
-				break;
-			}
-			
-		}
-		if (matched == 0) {
-			vector<structure> newEqClass;
-			newEqClass.push_back(optKS[i]);
-			eqClasses_coord.push_back(newEqClass);
-		}
-	}
-
-
-	
-
-	cout << "\t+ Comparison of coordinates" << endl;
-	cout << "\t\tEquality classes: " << eqClasses_coord.size() << endl;
-	cout << "\t\tTiming: " << T.timing() << " s" << endl << endl;
 
 
 
 
-	vector< vector<structure> > eqClasses_graph;
-	eqClasses_graph.push_back(optKS0);
-	for (vector<structure>::size_type i = 1; i < optKS.size(); i++) {
-		bool matched(0);
-		structure::undirectedGraph g0 = optKS[i].createGraph(p);
-		for (vector<structure>::size_type j = 0; j < eqClasses_graph.size(); j++) {
-			structure::undirectedGraph g1 = optKS[j].createGraph(p);
-
-			typename boost::property_map<structure::undirectedGraph, boost::vertex_index_t>::type
-			v0_index_map = boost::get (boost::vertex_index, g0);
-			//v1_index_map = boost::get (boost::vertex_index, test1);
-
-			vector<boost::graph_traits<structure::undirectedGraph>::vertex_descriptor> f(num_vertices(g0));
-	
-			if (boost::isomorphism (g0, g1, boost::isomorphism_map (make_iterator_property_map(f.begin(), v0_index_map, f[0])))) {
-				eqClasses_graph[j].push_back(optKS[i]);
-				matched = 1;
-				break;
-			}
-		}
-		if (matched == 0) {
-			vector<structure> newEqClass;
-			newEqClass.push_back(optKS[i]);
-			eqClasses_graph.push_back(newEqClass);
-		}
-
-	}
-
-
-	cout << "\t+ Graph isomorphism" << endl;
-	cout << "\t\tEquality classes: " << eqClasses_graph.size() << endl;
-	cout << "\t\tTiming: " << T.timing() << " s" << endl << endl;
-
-
+    
 	//compare function for vectors of doubles
 	auto compare_vector_double = [&] (vector<double> a, vector<double> b) {
 		assert (a.size() == b.size());
@@ -318,38 +192,6 @@ int main (int argc, char *argv[]) {
 		return false;
 	};
 
-
-	//post analysis of distance vector
-	vector< vector<structure> > eqClasses_coord_dist;
-	eqClasses_coord_dist.push_back(eqClasses_coord[0]);
-	for (vector< vector<structure> >::size_type i = 1; i < eqClasses_coord.size(); i++) {
-		bool matched(0);
-		for (vector< vector<structure> >::size_type j = 0; j < eqClasses_coord_dist.size(); j++) {
-			if (compare_vector_double (eqClasses_coord[i][0].getInterPartDist(), eqClasses_coord_dist[j][0].getInterPartDist())) {
-				eqClasses_coord_dist[j].insert (eqClasses_coord_dist[j].end(), eqClasses_coord[i].begin(), eqClasses_coord[i].end());
-				matched = 1;
-				break;
-			}
-		}
-		if (matched == 0) {
-			eqClasses_coord_dist.push_back(eqClasses_coord[i]);
-		}
-	}
-
-
-	vector<structure> structure0;
-	for (vector< vector<structure> >::size_type i = 0; i < eqClasses_coord_dist.size(); i++) {
-		structure0.push_back(eqClasses_coord_dist[i][0]);
-		vector<double> inertia = eqClasses_coord_dist[i][0].getMomentOfInertia();
-		stringstream in;
-		in << inertia << endl;
-		xyzout (eqClasses_coord[i][0], in.str() + to_string(i) + ".xyz");
-	}
-
-	cout << "\t+ Comparison of coordinates and distance vector" << endl;
-	cout << "\t\tEquality classes: " << eqClasses_coord_dist.size() << endl;
-	cout << "\t\tTiming: " << T.timing() << " s" << endl << endl;
-	
 
 
 
@@ -429,14 +271,6 @@ int main (int argc, char *argv[]) {
 	energystats << endl << "Number of unique structures: " << energyStat.size() << endl;
 	energystats << "Number of unique non-minimum structures: " << notMinimumStat.size() << endl;
 	energystats << "Number of unique minimum structures: " << energyStat.size()-notMinimumStat.size() << endl;
-	//for (vector<int>::size_type i = 0; i < notMinimum.size(); i++) {
-	//	vector<structure>::iterator printThis = find_if (optKS.begin(), optKS.end(), [&] (structure toPrint) { return (toPrint.getNumber() == notMinimum[i]); });
-	//	energystats << setw(10) << notMinimum[i] << setw(10) << printThis->getEnergy() << setw(10) << printThis->getMomentOfInertia();
-	//	for (int j=0; j<10; j++) {
-	//		energystats << ", " << printThis->getHessian()[j];
-	//	}
-	//	energystats << "..." << endl;
-	//}	
 
 	energystats << endl << "Number of warnings for Hessian eigenvalues: " << hessianWarnings << endl;
 	energystats << "List of non-minimum Structures: " << notMinimum << endl;
@@ -479,23 +313,6 @@ int main (int argc, char *argv[]) {
 			eqClasses_dist.push_back(newEqClass);
 		}
 	}
-	//ofstream similarStructures;
-	//similarStructures.open("similar");
-	//for (vector< vector<structure> >::size_type i = 0; i < eqClasses_dist.size(); i++) {
-	//	if (eqClasses_dist[i].size() > 1) {
-	//		for (vector<structure>::size_type j = 0; j < eqClasses_dist[i].size(); j++) {
-	//			xyzout (eqClasses_dist[i][j],"eq" + to_string(i) + "N" + to_string(j) + ".xyz");
-	//			vector<coord3d> currentCoord = eqClasses_dist[i][j].getCoordinates();
-	//			vector<double> currentInertia = eqClasses_dist[i][j].getMomentOfInertia();
-	//			vector<double> currentInterPartDist = eqClasses_dist[i][j].getInterPartDist();
-	//			for (vector<coord3d>::size_type k = 0; k < currentCoord.size(); k++) {
-	//				similarStructures << currentCoord[k] << endl;
-	//			}
-	//			similarStructures << endl << currentInertia << endl << currentInterPartDist << endl << endl;
-	//		}	
-	//	}
-	//}
-	//similarStructures.close();
 
 
 	cout << "\t+ Particle distance analysis" << endl;
@@ -505,10 +322,185 @@ int main (int argc, char *argv[]) {
 
 
 
-
-
 	
+	
+	cout << "\t+ Total rumtime: " << T.total_timing() << " s" << endl;
+    return 0; 
 
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//OLD FUNCTIONS NOT IN USE
+
+/*
+	//SORT BY direct structure comparison
+	vector< vector<structure> > eqClasses_coord;
+	eqClasses_coord.push_back(optKS0);
+	for (vector<structure>::size_type i = 1; i < optKS.size(); i++) {
+		bool matched(0);
+		for (vector< vector<structure> >::size_type j = 0; j < eqClasses_coord.size(); j++) {
+			if (optKS[i].compareCoordinates(eqClasses_coord[j][0])) {
+				eqClasses_coord[j].push_back(optKS[i]);
+				matched = 1;
+				break;
+			}
+			structure sig0;
+			vector<coord3d> coord_sig0 = optKS[i].sig(0);
+			sig0.setCoordinates(coord_sig0);
+			if (sig0.compareCoordinates(eqClasses_coord[j][0])) {
+				eqClasses_coord[j].push_back(optKS[i]);
+				matched = 1;
+				break;
+			}
+			structure sig1;
+			vector<coord3d> coord_sig1 = optKS[i].sig(1);
+			sig1.setCoordinates(coord_sig1);
+			if (sig1.compareCoordinates(eqClasses_coord[j][0])) {
+				eqClasses_coord[j].push_back(optKS[i]);
+				matched = 1;
+				break;
+			}
+			structure sig2;
+			vector<coord3d> coord_sig2 = optKS[i].sig(2);
+			sig2.setCoordinates(coord_sig2);
+			if (sig2.compareCoordinates(eqClasses_coord[j][0])) {
+				eqClasses_coord[j].push_back(optKS[i]);
+				matched = 1;
+				break;
+			}
+			structure c201;
+			vector<coord3d> coord_c201 = optKS[i].c2(0,1);
+			c201.setCoordinates(coord_c201);
+			if (c201.compareCoordinates(eqClasses_coord[j][0])) {
+				eqClasses_coord[j].push_back(optKS[i]);
+				matched = 1;
+				break;
+			}
+			structure c202;
+			vector<coord3d> coord_c202 = optKS[i].c2(0,2);
+			c202.setCoordinates(coord_c202);
+			if (c202.compareCoordinates(eqClasses_coord[j][0])) {
+				eqClasses_coord[j].push_back(optKS[i]);
+				matched = 1;
+				break;
+			}
+			structure c212;
+			vector<coord3d> coord_c212 = optKS[i].c2(1,2);
+			c212.setCoordinates(coord_c212);
+			if (c212.compareCoordinates(eqClasses_coord[j][0])) {
+				eqClasses_coord[j].push_back(optKS[i]);
+				matched = 1;
+				break;
+			}
+			structure inverse;
+			vector<coord3d> coord_inv = optKS[i].inv();
+			inverse.setCoordinates(coord_inv);
+			if (inverse.compareCoordinates(eqClasses_coord[j][0])) {
+				eqClasses_coord[j].push_back(optKS[i]);
+				matched = 1;
+				break;
+			}
+			
+		}
+		if (matched == 0) {
+			vector<structure> newEqClass;
+			newEqClass.push_back(optKS[i]);
+			eqClasses_coord.push_back(newEqClass);
+		}
+	}
+
+
+
+	cout << "\t+ Comparison of coordinates" << endl;
+	cout << "\t\tEquality classes: " << eqClasses_coord.size() << endl;
+	cout << "\t\tTiming: " << T.timing() << " s" << endl << endl;
+*/
+
+
+/*
+	vector< vector<structure> > eqClasses_graph;
+	eqClasses_graph.push_back(optKS0);
+	for (vector<structure>::size_type i = 1; i < optKS.size(); i++) {
+		bool matched(0);
+		structure::undirectedGraph g0 = optKS[i].createGraph(p);
+		for (vector<structure>::size_type j = 0; j < eqClasses_graph.size(); j++) {
+			structure::undirectedGraph g1 = optKS[j].createGraph(p);
+
+			typename boost::property_map<structure::undirectedGraph, boost::vertex_index_t>::type
+			v0_index_map = boost::get (boost::vertex_index, g0);
+			//v1_index_map = boost::get (boost::vertex_index, test1);
+
+			vector<boost::graph_traits<structure::undirectedGraph>::vertex_descriptor> f(num_vertices(g0));
+	
+			if (boost::isomorphism (g0, g1, boost::isomorphism_map (make_iterator_property_map(f.begin(), v0_index_map, f[0])))) {
+				eqClasses_graph[j].push_back(optKS[i]);
+				matched = 1;
+				break;
+			}
+		}
+		if (matched == 0) {
+			vector<structure> newEqClass;
+			newEqClass.push_back(optKS[i]);
+			eqClasses_graph.push_back(newEqClass);
+		}
+
+	}
+
+
+	cout << "\t+ Graph isomorphism" << endl;
+	cout << "\t\tEquality classes: " << eqClasses_graph.size() << endl;
+	cout << "\t\tTiming: " << T.timing() << " s" << endl << endl;
+*/
+
+
+/*
+	//post analysis of distance vector
+	vector< vector<structure> > eqClasses_coord_dist;
+	eqClasses_coord_dist.push_back(eqClasses_coord[0]);
+	for (vector< vector<structure> >::size_type i = 1; i < eqClasses_coord.size(); i++) {
+		bool matched(0);
+		for (vector< vector<structure> >::size_type j = 0; j < eqClasses_coord_dist.size(); j++) {
+			if (compare_vector_double (eqClasses_coord[i][0].getInterPartDist(), eqClasses_coord_dist[j][0].getInterPartDist())) {
+				eqClasses_coord_dist[j].insert (eqClasses_coord_dist[j].end(), eqClasses_coord[i].begin(), eqClasses_coord[i].end());
+				matched = 1;
+				break;
+			}
+		}
+		if (matched == 0) {
+			eqClasses_coord_dist.push_back(eqClasses_coord[i]);
+		}
+	}
+
+
+	vector<structure> structure0;
+	for (vector< vector<structure> >::size_type i = 0; i < eqClasses_coord_dist.size(); i++) {
+		structure0.push_back(eqClasses_coord_dist[i][0]);
+		vector<double> inertia = eqClasses_coord_dist[i][0].getMomentOfInertia();
+		stringstream in;
+		in << inertia << endl;
+		xyzout (eqClasses_coord[i][0], in.str() + to_string(i) + ".xyz");
+	}
+
+	cout << "\t+ Comparison of coordinates and distance vector" << endl;
+	cout << "\t\tEquality classes: " << eqClasses_coord_dist.size() << endl;
+	cout << "\t\tTiming: " << T.timing() << " s" << endl << endl;
+*/	
+
+/*
 	//sort all minimum structures into vector< vector<structure> > according to squared adj matrix
 	vector< vector<structure> > eqClasses_adjMat2;
 	eqClasses_adjMat2.push_back(optKS0);
@@ -532,10 +524,9 @@ int main (int argc, char *argv[]) {
 	cout << "\t+ Bond number analysis" << endl;
 	cout << "\t\tEquality classes: " << eqClasses_adjMat2.size() << endl;
 	cout << "\t\tTiming: " << T.timing() << " s" << endl << endl;
+*/
 
-	
-
-	
+/*	
 	//sort all minimum structures into vector< vector<structure> > according to adj matrix eigenvalues
 	vector< vector<structure> > eqClasses_adjMatEv;
 	eqClasses_adjMatEv.push_back(optKS0);
@@ -559,48 +550,4 @@ int main (int argc, char *argv[]) {
 	cout << "\t+ Adjacency matrix eigen analysis" << endl;
 	cout << "\t\tEquality classes: " << eqClasses_adjMatEv.size() << endl;
 	cout << "\t\tTiming: " << T.timing() << " s" << endl << endl;
-
-
-
-
-
-
-
-
-
-	////WRITE OUTPUT STRUCTURES
-	//switch (output_switch) {
-	//	case 1:
-	//		if (structureNumbers[0] == 0) {
-	//			for (vector<structure>::size_type i = 0; i < optKS.size(); i++) {
-	//				xyzout (optKS[i], "optStructure" + to_string (optKS[i].getNumber()));
-	//				xyzout (allKS[i], "inpStructure" + to_string (allKS[i].getNumber()));
-	//			}
-	//		}
-	//		else {
-	//			for (vector<int>::size_type i = 0; i < structureNumbers.size(); i++) {
-	//				vector<structure>::iterator printThis = find_if (optKS.begin(), optKS.end(), [&] (structure toPrint) { return (toPrint.getNumber() == structureNumbers[i]); });
-	//				if (printThis != optKS.end()) {
-	//					xyzout (*printThis, "optStructure" + to_string (printThis->getNumber()));
-	//					xyzout (allKS[structureNumbers[i - 1]], "inpStructure" + to_string (allKS[i].getNumber()));
-	//				}
-	//				else {
-	//					cerr << "Structure number " << structureNumbers[i] << " not found." << endl;
-	//				}
-	//			}
-	//		}
-	//	default:
-	//		break;
-	//}
-
-
-
-
-
-	
-	
-	cout << "\t+ Total rumtime: " << T.total_timing() << " s" << endl;
-    return 0; 
-
-    
-}
+*/
